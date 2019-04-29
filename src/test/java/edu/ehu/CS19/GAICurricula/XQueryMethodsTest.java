c470d6e691cffdfb703d3d833e23f7af2a209e09
FosterGun <fostergun123@gmail.com>
2019-04-30 01:55:07 +0200

    Escritos test para el proyecto

--- src/test/java/edu/ehu/CS19/GAICurricula/XQueryMethodsTest.java
+++ src/test/java/edu/ehu/CS19/GAICurricula/XQueryMethodsTest.java
@@ -9,6 +9,8 @@ import org.junit.Test;
 
 public class XQueryMethodsTest {
 	
+	XQueryMethods xqm;
+	
 	String listaXQueryFR;
 	String listaXQueryFR2;
 	String listaXQueryPOM;
@@ -18,17 +20,20 @@ public class XQueryMethodsTest {
 	
 	@Before
 	public void setUp() throws Exception {
+		
+		xqm = new XQueryMethods();
+		
 		listaXQueryFR = 
-				"for $c in doc(\"input/curriculumFranceSimp.xml\")/curriculums/curriculum[./prenom = 'Gusteau']" +
-				"return $c/erasmusendroits/lieu";
+				"for $c in doc(\"input/curriculumFranceSimp.xml\")/curriculums/curriculum[./prenom = 'Gusteau']\n" +
+				"return $c/erasmusendroits/lieu/text()";
 
 		listaXQueryFR2 = 
-				"for $c in doc(\"input/curriculumFranceSimp.xml\")/curriculums/curriculum[./prenom = 'Parsia']" +
-				"return $c/erasmusendroits/lieu";
+				"for $c in doc(\"input/curriculumFranceSimp.xml\")/curriculums/curriculum[./prenom = 'Parsia']\n" +
+				"return $c/erasmusendroits/lieu/text()";
 		
 		listaXQueryPOM = 
-				"for $d in doc(\"input/pom.xml\")/project/dependencies/dependency" +
-				"return $d/artifactId";
+				"for $d in doc(\"input/pom.xml\")/project/dependencies/dependency\n" +
+				"return $d/artifactId/text()";
 		
 		esperadoFR = new ArrayList<String>();
 		esperadoFR.add("Bilbao");
@@ -41,9 +46,51 @@ public class XQueryMethodsTest {
 		
 	}
 
-
+	/**
+	 *  Comprueba que los lugares del primer estudiante francés son los correctos.
+	 */
 	@Test
 	public void consultaListaXQueryTestFR() {
+		ArrayList<String> resultado = xqm.consultaListaXQuery(listaXQueryFR);
+		
+		for(int i = 0; i < resultado.size(); i++) {
+			if(resultado.get(i).compareTo(esperadoFR.get(i)) != 0) {
+				fail("El resultado y lo esperado es distinto: " + resultado.get(i) + " con " + esperadoFR.get(i));
+			}
+		}
+		
+		assertTrue(true);
+	}
+	
+	/**
+	 *  Comprueba que los lugares del segundo estudiante francés son los correctos.
+	 */
+	@Test
+	public void consultaListaXQueryTestFR2() {
+		ArrayList<String> resultado = xqm.consultaListaXQuery(listaXQueryFR2);
+		
+		for(int i = 0; i < resultado.size(); i++) {
+			if(resultado.get(i).compareTo(esperadoFR.get(i)) != 0) {
+				fail("El resultado y lo esperado es distinto: " + resultado.get(i) + " con " + esperadoFR.get(i));
+			}
+		}
+		
+		assertTrue(true);
+	}
+	
+	/**
+	 *  Comprueba que las dependencias del POM tienen un artifactId correcto.
+	 */
+	@Test
+	public void consultaListaXQueryTestPOM() {
+		ArrayList<String> resultado = xqm.consultaListaXQuery(listaXQueryPOM);
+		
+		for(int i = 0; i < resultado.size(); i++) {
+			if(resultado.get(i).compareTo(esperadoPOM.get(i)) != 0) {
+				fail("El resultado y lo esperado es distinto: " + resultado.get(i) + " con " + esperadoPOM.get(i));
+			}
+		}
+		
 		assertTrue(true);
 	}
 
